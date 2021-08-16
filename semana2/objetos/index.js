25458eb83dba9414c27d790af439c355dec4c8a2
Willian Santiago <shane.santiago1@hotmail.com>
2021-08-16 14:29:00 -0300

    Semana 2 aula 07

---
+++ semana2/objetos/index.js
@@ -0,0 +1,52 @@
+// Exercicio 1
+// "Matheus Nachtergaele"
+
+// "Virginia Cavendish"
+
+// {canal: "Globo", horario: "14h"}
+
+// Exercicio 2
+// Juca
+// Juba
+// Jubo
+
+// A sintaxe copia o objeto e faz alteração
+
+// Exercicio 3
+// False
+// A função backender tá atribuida como falso e retornou esse valor
+// A altura não foi definida
+
+// const pessoa = {
+//     nome: "Teste",
+//     apelidos: ["Testando", "Testado", "Testarei"]
+// }
+// console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de ${pessoa.apelidos[0]}, ${pessoa.apelidos[1]} ou ${pessoa.apelidos[2]}`)
+
+// const novaPessoa = {
+//     pessoaCitada: pessoa.nome,
+//     novosApelidos: ["Testado 1", "Testado 2", "Testado 3"],
+//     ...pessoa
+// }
+// console.log(novaPessoa)
+
+// const pessoa = {
+//     nome: "Teste",
+//     idade: 50,
+//     profissao: "Programador"
+// }
+// let resultado = pessoa
+// console.log(pessoa.nome, pessoa.nome.length, pessoa.idade, pessoa.profissao, pessoa.profissao.length)
+
+// const carrinho = []
+// const frutas = [
+//     {fruta: "Abacaxi", disponibilidade: true},
+//     {fruta: "Pera", disponibilidade: true},
+//     {fruta: "Maca", disponibilidade: true}
+// ]
+// umaFuncao = carrinho
+// umaFuncao.push(frutas[0])
+// umaFuncao.push(frutas[1])
+// umaFuncao.push(frutas[2])
+// console.log(carrinho)
+
