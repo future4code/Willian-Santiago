c063b1b8b6abf6444e80ae5cff72ce3fc27f9833
Willian Santiago <shane.santiago1@hotmail.com>
2021-11-16 14:32:05 -0300

    Exercicio aula 41

---
+++ Aula-Node/Exercicio1/index.js
@@ -0,0 +1,9 @@
+const nome = "Teste"
+const idade = 48
+
+console.log(`Olá, ${nome}. Você tem ${idade} anos`)
+
+const soma = idade + 8
+
+console.log(`Ola, ${nome}. Você tem ${idade} anos. Em 8 anos você terá ${soma}`)
+
---
+++ Aula-Node/Exercicio2/index.js
@@ -0,0 +1,7 @@
+const valor1 = process.argv[2] = 5
+const valor2 = process.argv[3] = 5
+
+soma = valor1 + valor2
+
+
+console.log(soma)
