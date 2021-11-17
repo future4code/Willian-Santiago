5ac9346940010420594df2dffa00f6d9ed26806b
Willian Santiago <shane.santiago1@hotmail.com>
2021-11-17 13:49:40 -0300

    Aula42 typescript

---
+++ Aula43/src/exercicio-4.ts
@@ -0,0 +1,26 @@
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+// Escreveria o comando tsc src/exercicio-4.ts
+// Se o arquivo não estiver na pasta screen, basta apenas usar o comando tsc exercicio-4.ts
